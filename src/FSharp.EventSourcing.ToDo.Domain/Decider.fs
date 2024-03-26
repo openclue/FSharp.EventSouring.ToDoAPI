93aa4db7e22a9db59da1e384c940638fa43d2bda
Daniel Jabłoński <d.jablonski@openclue.pl>
2024-03-26 17:51:14 +0100

    Refactor Decider logic into separate module
    The structure of the Decider logic has been refactored to improve organization and readability of the code. The Decider type and its related functions have been moved inside a separate module. The TaskDecider has been adjusted accordingly to align with these changes.

--- src/FSharp.EventSourcing.ToDo.Domain/Decider.fs
+++ src/FSharp.EventSourcing.ToDo.Domain/Decider.fs
@@ -1,35 +1,40 @@
-module FSharp.EventSourcing.ToDo.Domain.Decider
+namespace FSharp.EventSourcing.ToDo.Domain
 
 open FSharp.EventSourcing.ToDo.Domain.Types
 open FSharp.EventSourcing.ToDo.Domain.Functions
 
-type Decider<'C, 'E, 'S, 'ERR> =
-    { decide: 'C -> 'S -> Result<'E, 'ERR>
-      evolve: 'S -> 'E -> 'S
-      initialState: 'S
-      isComplete: 'S -> bool }
+module Decider =
 
-let decide (command: TaskCommand) (state: TaskState) =
-    match command with
-    | CreateTask c -> handleCreateTaskCommand c state
-    | AssignTask c -> handleAssignTaskCommand c state
-    | AddComment c -> handleAddCommentCommand c state
-    | CompleteTask c -> handleCompleteTaskCommand c state
+    type Decider<'C, 'E, 'S, 'ERR> =
+        { decide: 'C -> 'S -> Result<'E, 'ERR>
+          evolve: 'S -> 'E -> 'S
+          initialState: 'S
+          isComplete: 'S -> bool }
 
-let evolve (state: TaskState) (event: TaskEvent) =
-    match state, event with
-    | Empty, TaskCreated e -> createTask e
-    | Open task, TaskCompleted e -> completeTask task e
-    | Open task, _ -> TaskState.Open <| applyEvent task event 
-    | _ -> state
+    let decide (command: TaskCommand) (state: TaskState) =
+        match command with
+        | CreateTask c -> handleCreateTaskCommand c state
+        | AssignTask c -> handleAssignTaskCommand c state
+        | AddComment c -> handleAddCommentCommand c state
+        | CompleteTask c -> handleCompleteTaskCommand c state
 
-let isComplete (state: TaskState) =
-    match state with
-    | Completed _ -> true
-    | _ -> false
+    let evolve (state: TaskState) (event: TaskEvent) =
+        match state, event with
+        | Empty, TaskCreated e -> createTask e
+        | Open task, TaskCompleted e -> completeTask task e
+        | Open task, _ -> TaskState.Open <| applyEvent task event
+        | _ -> state
 
-let TaskDecider =
-    { decide = decide
-      evolve = evolve
-      initialState = Empty
-      isComplete = isComplete }
+    let isComplete (state: TaskState) =
+        match state with
+        | Completed _ -> true
+        | _ -> false
+
+module Task =
+    open Decider
+
+    let decider =
+        { decide = decide
+          evolve = evolve
+          initialState = Empty
+          isComplete = isComplete }
